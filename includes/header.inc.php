a7bdfd32cd8d8c014e1b7009565694e583ed7d0c
Daniel Devine <x15004091@student.ncirl.ie>
2016-11-26 16:22:02 +0000

    station

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -29,7 +29,7 @@
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <!--<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -52,7 +52,7 @@
           </form>
         </div><!--/.navbar-collapse -->
       </div>
-    </nav>
+    </nav>-->
 
 TAG
 

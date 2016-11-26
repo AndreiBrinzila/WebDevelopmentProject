970de999571cdf9bd31a61c3bf4de92a9f133837
Daniel Devine <x15004091@student.ncirl.ie>
2016-11-26 16:12:51 +0000

    1612

--- js/main.js
+++ js/main.js
@@ -2,10 +2,30 @@
 $(document).ready(function(){ 
 	$.ajax({ 
 		type: "GET",
-		url: "/_data/getAllStations.xml",
+		url: "_data/getAllStations.xml",
 		dataType: "xml",
 		success: function(xml) { 
-			$(xml).each()
-		};
+		/*
+			$.each(xml.find("StationDesc"), function(){
+				console.log(this.index + " : " + this.stationName);
+			});
+		
+			$.parseXML(xml).find('objStation').each(function(index){
+				var stationName = $(this).find('StationDesc').text();
+				console.log(stationName);
+			});
+		*/
+		
+			$(xml).find("StationDesc").each(function(index, stationName) {
+				console.log(index + " : " + stationName);
+			});
+		
+			//console.log($(xml).find("StationDesc"));
+		}
 	});
-}
+});
+
+//var xml = "//_data/getAllStations.xml";
+
+//var xmlDoc = $.parseXML(xml);
+
